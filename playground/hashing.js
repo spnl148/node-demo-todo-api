7307000cc412e319fabb2eb8759e758c0ebebc68
shashvat <laneteam.shashvat@gmail.com>
2018-01-22 14:42:11 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,29 @@
 
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    'name' :'SPnL'
-};
 
-var token = jwt.sign(data,'mine');
-console.log(token);
+var password = 'abc123'
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password,salt, (err,hash) =>{
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token,'mine');
-console.log('Decoded',decoded)
+bcrypt.compare(password,'$2a$10$Rjbds51bqlgwf2IZZy1sxO4h1yMxkUBzLVX1toEcXqRC1gd4mcX16',(e,h) =>{
+    console.log(h)
+})
+
+// var data = {
+//     'name' :'SPnL'
+// };
+
+// var token = jwt.sign(data,'mine');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'mine');
+// console.log('Decoded',decoded)
 // var data = 'Testing'
 // var HashData = SHA256(data).toString();
 
