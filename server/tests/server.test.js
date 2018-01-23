0e993bcddb71e0dbf852e0afffb47efcd04e030d
shashvat <laneteam.shashvat@gmail.com>
2018-01-23 10:31:33 +0530

    Add test for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -201,3 +201,44 @@ describe('POST /users', () => {
             .end(done);
     });
 });
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+
+        request(app)
+            .post('/users/login')
+            .send({ 'email': users[1].email, 'password': users[1].password })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toBeTruthy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).not.toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should return invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({ 'email': users[1].email, 'password': users[1].password+'1' })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).not.toBeTruthy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
