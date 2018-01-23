aac73fa7e63c1e76e5c5be6a9ced8e664b714c19
shashvat <laneteam.shashvat@gmail.com>
2018-01-23 14:16:32 +0530

    Done with Security and Authentication

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,7 +98,7 @@ describe('DELETE /todos/id:', () => {
                 if (err)
                     return done(err);
                 Todo.findById(hexId).then((todo)=>{
-                    expect(todo).not.toBeTruthy();
+                    expect(todo).toBeFalsy();
                     done();
                 }).catch((e)=> done(e));
             });
@@ -185,6 +185,7 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
                 expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBeFalsy();
             })
             .end(done);
     });
@@ -271,7 +272,12 @@ describe('POST /users/login', () => {
                     return done(err);
                 }
                 User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens.length).not.toBe(0);
+                    // expect(user.tokens.length).not.toBe(0);
+
+                    expect(user.toObject().tokens[1]).toMatchObject({
+                        access:'auth',
+                        token: res.header['x-auth']
+                    });
                     done();
                 }).catch((e) => done(e));
             });
@@ -283,7 +289,7 @@ describe('POST /users/login', () => {
             .send({ 'email': users[1].email, 'password': users[1].password + '1' })
             .expect(400)
             .expect((res) => {
-                expect(res.headers['x-auth']).not.toBeTruthy();
+                expect(res.headers['x-auth']).toBeFalsy();
             })
             .end((err, res) => {
                 if (err) {
